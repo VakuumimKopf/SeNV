0869c385ac3aca04385885ff5f238836e2a5633d
VakuumimKopf <oliverlorenz.de@gmail.com>
2025-04-21 13:12:56 +0200

    Erster Action Server-Client von lane_con zu intersection_con

--- src/senv/senv/intersection_con.py
+++ src/senv/senv/intersection_con.py
@@ -1,7 +1,9 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from rclpy.action import ActionServer
+from rclpy.action.server import ServerGoalHandle
 from senv_interfaces.msg import Pic,Laser 
+from senv_interfaces.action import ConTask
 
 
 class intersection_con(Node):
@@ -33,6 +35,30 @@ class intersection_con(Node):
         )
         self.subscriber_laser  # prevent unused variable warning
 
+        self.intersection_task_server_ = ActionServer(
+            self,
+            ConTask,
+            "intersection_task",
+            self.execute_callback
+        )
+
+    def execute_callback(self, goal_handle: ServerGoalHandle):
+        
+        # Get request from goal
+        target = goal_handle.request.start_working
+        self.get_logger().info("starting intersection server")
+
+        # Execute action 
+        self.turned_on = target
+        self.datahandler()
+
+        # Final Goal State
+        goal_handle.succeed()
+
+        #Result
+        result = ConTask.Result()
+        result.finished = True
+        return result
 
     def pic_callback(self, msg):
         if self.turned_on == False:
@@ -47,6 +73,9 @@ class intersection_con(Node):
         # Define your callback function here
         self.get_logger().info('Received message laser')
         # obstacle avoidance
+
+    def datahandler(self):
+        self.get_logger().info("Handling intersection data")
 
 
 def main(args=None):
--- src/senv/senv/lane_con.py
+++ src/senv/senv/lane_con.py
@@ -1,7 +1,9 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from rclpy.action import ActionClient
+from rclpy.action.client import ClientGoalHandle
 from senv_interfaces.msg import Pic,Laser
+from senv_interfaces.action import ConTask
 
 
 class lane_con(Node):
@@ -13,6 +15,7 @@ class lane_con(Node):
         qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
                                           history=rclpy.qos.HistoryPolicy.KEEP_LAST,depth=1)
 
+        # Subscribing to camera and laser topics
         self.subscriber_pic = self.create_subscription(
             Pic,  # Replace with the actual message type
             'pic',
@@ -28,6 +31,35 @@ class lane_con(Node):
             qos_profile=qos_policy
         )
         self.subscriber_laser  # prevent unused variable warning
+
+        self.intersection_client_ = ActionClient(
+            self,
+            ConTask,
+            "intersection_task"
+        )
+
+    # Sending request to action server 
+    def send_goal(self, working_status):
+        self.intersection_client_.wait_for_server()
+
+        goal = ConTask.Goal()
+        goal.start_working = working_status
+        
+        self.get_logger().info("sending data to intersection server")
+        self.intersection_client_. \
+            send_goal_async(goal). \
+                add_done_callback(self.goal_response_callback)
+
+    # Awaiting response from action server if request accepted 
+    def goal_response_callback(self, future):
+        self.goal_handle_: ClientGoalHandle = future.result()
+        if self.goal_handle_.accepted: # if accepted call goal_result_callback
+            self.goal_handle_.get_result_async().add_done_callback(self.goal_result_callback)
+
+    # Requesting Result from action server
+    def goal_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info("Result:" + str(result.finished))
         
     def pic_callback(self, msg):
         # Process the incoming message
@@ -40,9 +72,11 @@ class lane_con(Node):
         # obstacle avoidance
 
 
+
 def main(args=None):
     rclpy.init(args=args)
     node = lane_con()
+    node.send_goal(True)
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
