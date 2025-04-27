7e5c23d55913ba2a3d9267e43b3baac5de6450dc
n-laaser <pokekristall.fan@gmail.com>
2025-04-27 12:22:23 +0200

    Merged Gerüst with aufgabe 1, fixed missing sections

--- src/senv/senv/lane_con.py
+++ src/senv/senv/lane_con.py
@@ -1,109 +1,21 @@
-<<<<<<< HEAD
-import rclpy 
-import rclpy.node
-import cv2
-import numpy as np
 
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import CompressedImage
-from cv_bridge import CvBridge, CvBridgeError
-
-class lane_con(rclpy.node.Node):
-=======
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from rclpy.action.client import ClientGoalHandle
 from senv_interfaces.msg import Pic,Laser
 from senv_interfaces.action import ConTask
->>>>>>> c16411254d4dbc1762a597ba784d2dfd2d3f7ad0
-
+from geometry_msgs.msg import Twist
+import numpy as np
+#
+class lane_con(rclpy.node.Node):
     def __init__(self):
         super().__init__('follower')
         
-<<<<<<< HEAD
-        # definition of the parameters that can be changed at runtime
-        self.declare_parameter('boundary_left', 100) # 200 für 640px, 100 für 320
-        self.declare_parameter('boundary_right', 630) # 440 für 640px, 220 für 320px
-        self.declare_parameter('threshold_line', 100) #is now light_lim
-        self.declare_parameter('speed_drive', 0.115)
-        self.declare_parameter('speed_turn', 0.3)
-        self.declare_parameter('light_lim', 100)
-        self.declare_parameter('middle_tol', 20)
-        self.declare_parameter('speed_turn_adjust',0.3)
-        self.lineposition = 0
-
-        # init openCV-bridge
-        self.bridge = CvBridge()
-
-        #self.img_row = np.array([0, 64, 128, 192, 255], dtype=np.uint8) # Beispiel
-        self.img_row = np.random.randint(0, 256, 640, dtype=np.uint8)
-
-        # definition of the QoS in order to receive data despite WiFi
-        qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
-                                          history=rclpy.qos.HistoryPolicy.KEEP_LAST,
-                                          depth=1)
-        
-        self.last_spin = False # False == gegen Uhrzeigersinn True==mit Uhrzeigersinn
-        # create subscribers for image data with changed qos
-        self.subscription = self.create_subscription(
-            CompressedImage,
-            '/image_raw/compressed',
-            self.scanner_callback,
-            qos_profile=qos_policy)
-        self.subscription  # prevent unused variable warning
-
-        # create publisher for driving commands
-        self.publisher_lane_con = self.create_publisher(Twist, 'line', 1)
-
-        # create timer to periodically invoke the driving logic
-        self.timer_period = 0.1  # seconds
-        self.my_timer = self.create_timer(self.timer_period, self.timer_callback)
-    
-    # handling received image data
-    def scanner_callback(self, data):
-        # convert message to opencv image
-        img_cv = self.bridge.compressed_imgmsg_to_cv2(data, desired_encoding = 'passthrough')
-
-        # convert image to grayscale
-        img_gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
-
-        # get image size
-        height, width = img_gray.shape[:2]
-        #self.get_logger().info(f"Image Höhe: {width} ")
-        # get the lowest row from image
-        img_row = img_gray[height-9,:]
-        self.img_row = img_row 
-
-        # show image
-        cv2.imshow("IMG", img_gray)
-        cv2.imshow("IMG_ROW", img_row)
-        cv2.waitKey(1)
-
-
-    # driving logic for lane_coning
-    def timer_callback(self):
-        #self.get_logger().info("FollowerLogic")
-        
-        boundary_left = self.get_parameter('boundary_left').get_parameter_value().integer_value
-        boundary_right = self.get_parameter('boundary_right').get_parameter_value().integer_value
-        speed_drive = self.get_parameter('speed_drive').get_parameter_value().double_value
-        speed_turn = self.get_parameter('speed_turn').get_parameter_value().double_value
-        speed_turn_adjust = self.get_parameter('speed_turn_adjust').get_parameter_value().double_value
-        light_lim = self.get_parameter('light_lim').get_parameter_value().integer_value
-        middle_tol = self.get_parameter('middle_tol').get_parameter_value().integer_value
-        last_spin = self.last_spin
-        #self.get_logger().info(f"Länge cam array: {len(img_row)}")
-        img_row_original = self.img_row
-        img_row = self.img_row[boundary_left:boundary_right]
-
-        #20 Größten Werte nach Größe geordnet
-        img_row_sorted = np.argsort(img_row)[-20:]
-=======
         # Qos policy setting
         qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
                                           history=rclpy.qos.HistoryPolicy.KEEP_LAST,depth=1)
-
+        self.last_spin = False
         # Subscribing to camera and laser topics
         self.subscriber_pic = self.create_subscription(
             Pic,  # Replace with the actual message type
@@ -157,10 +69,18 @@ from senv_interfaces.action import ConTask
 
     def laser_callback(self, msg):
         # Define your callback function here
+        boundary_left = 100
+        boundary_right = 630
+        speed_drive = 0.115
+        speed_turn = 0.3
+        light_lim = 100
+        last_spin = self.last_spin
         self.get_logger().info('Received message laser')
         # obstacle avoidance
->>>>>>> c16411254d4dbc1762a597ba784d2dfd2d3f7ad0
+        img_row = self.img_row[boundary_left:boundary_right]
 
+        #20 Größten Werte nach Größe geordnet
+        img_row_sorted = np.argsort(img_row)[-20:]
         # Werte der 20 größten Elemente
         largest_values = img_row[img_row_sorted]
 
@@ -173,7 +93,7 @@ from senv_interfaces.action import ConTask
         max_avg = np.mean(img_row_sorted)
         closest_value = img_row[middle_index_in_original]
         #middle_pix = 320 # für 640px x irgendwas
-        middle_pix = 550 # für 320px
+        middle_pix = 550 
         speed = 0.0
         turn = 0.0
         brightest = max(img_row)
@@ -227,13 +147,10 @@ from senv_interfaces.action import ConTask
 def main(args=None):
     rclpy.init(args=args)
     node = lane_con()
-<<<<<<< HEAD
-=======
     node.send_goal(True)
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
->>>>>>> c16411254d4dbc1762a597ba784d2dfd2d3f7ad0
 
     try:
         rclpy.spin(node)
